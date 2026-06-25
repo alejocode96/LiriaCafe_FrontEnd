2529b9f9a06d7935df704f14b1635a96bf0f42ac
Alejandro Galeano <alejocode96@email.com>
2026-06-25 11:01:30 -0500

    init project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+
+
+function App() {
+
+
+  return (
+    <>
+      <div className='bg-amber-300 justify-center'>
+        HOME
+      </div>
+    </>
+  )
+}
+
+export default App
